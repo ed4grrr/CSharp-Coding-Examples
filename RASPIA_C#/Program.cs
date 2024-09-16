87a445693da606af6772b7f9da53bf0000bf1a24
ed4grrr <edgarbowlinthethird@gmail.com>
2024-09-16 00:37:05 -0400

    Updated clean user Input

--- RASPIA_C#/Program.cs
+++ RASPIA_C#/Program.cs
@@ -19,7 +19,11 @@ namespace RASPIA_C_
         public string[] cleanUserInput(params string[] input)
         {
             //make user output http request friendly
-            return input.Select(element => Uri.EscapeDataString(element)).ToArray();
+            return input.Where(element => { if (element != null)
+                    { return true; }
+                else
+                { return false; } })
+                .Select(element => Uri.EscapeDataString(element)).ToArray();
             
         }
 
@@ -49,7 +53,13 @@ namespace RASPIA_C_
         {
            Main_API api = new Main_API();
 
-            string[] test =api.cleanUserInput(["Soon T M","edgar//3\\"]);
+            string[] answers = new string[24];
+            answers[0] = "Soon T M";
+            answers[1] = "edgar//3\\";
+
+
+
+            string[] test =api.cleanUserInput(answers);
             foreach (string input in test)
             {
                 Console.WriteLine(input);
