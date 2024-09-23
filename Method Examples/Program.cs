e01e3659f7fdc33fce75bd50c6a3f75fa01000d3
ed4grrr <edgarbowlinthethird@gmail.com>
2024-09-23 01:03:01 -0400

    Updated Method Examples

---
+++ Method Examples/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Examples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            beginnerMethod();
+
+            printLocalTime();
+
+            Console.ReadLine(); 
+        }
+
+
+
+        static void beginnerMethod()
+        {
+            Console.WriteLine($"You are printing from a method, and have successfully exceuted {System.Reflection.MethodBase.GetCurrentMethod().Name}!");
+        }
+        
+        static void printLocalTime()
+        {
+            Console.WriteLine($"You are printing from a method, and have successfully exceuted {System.Reflection.MethodBase.GetCurrentMethod().Name}!");
+            string currentDateAndTime = DateTime.Now.ToString();
+            Console.WriteLine($"The local time is {currentDateAndTime}");
+        }
+        
+        static string printUserInput()
+        {
+            Console.WriteLine($"You are printing from a method, and have successfully exceuted {System.Reflection.MethodBase.GetCurrentMethod().Name}!");
+            string userInput = Console.ReadLine();
+            return $"The local time is {userInput }";
+        }
+
+        static int ageInTwentyYears(int age)
+        {
+
+            return age + 20;
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
